e03eaea1a1b531fb2a5c91f6c89b0f10d4b50dd4
Manoid26 <salmanghrs@gmail.com>
2023-04-19 22:43:28 +0200

    pour tester lancer la commande php test.php

---
+++ testphp/param.inc.php
@@ -0,0 +1,5 @@
+<?php
+define("HOTE", "https://webetu.univ-st-etienne.fr/"); /* L'adresse du serveur */
+define("UTILISATEUR", "gs05790v"); /* Votre nom d'utilisateur */
+define("PASSE", "CH9LF7BW"); /* Votre mot de passe */
+?>
---
+++ testphp/test.php
@@ -0,0 +1,8 @@
+<?php
+/*Inclusion du fich*/
+require_once('connex.inc.php');
+
+/*connexion a ma base*/
+$pdo = connexion('gs05790v');
+
+?>
