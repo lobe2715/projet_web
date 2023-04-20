1936d830efe144c47428243d468a9304d3d9cd65
lobe2715 <ecrard2715@gmail.com>
2023-04-20 15:30:48 +0200

    accueuil

---
+++ javascrip/accueiljs.js
@@ -0,0 +1,8 @@
+function add() {
+    X = document.createElement('textarea');
+    X.setAttribute("id", "commentaire");
+    X.setAttribute("name", "commentaire");
+    X.setAttribute("style", "width:400px");
+    X.setAttribute("maxlength", 40);
+    mon_input_texte.appendChild(X) ;
+}
