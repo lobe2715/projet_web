a1e9e1e327e2dc378a904ead1657f6b1b3df7fb2
lobe2715 <ecrard2715@gmail.com>
2023-04-20 16:00:59 +0200

    postit

--- javascrip/accueiljs.js
+++ javascrip/accueiljs.js
@@ -2,7 +2,8 @@ function add() {
     X = document.createElement('textarea');
     X.setAttribute("id", "commentaire");
     X.setAttribute("name", "commentaire");
-    X.setAttribute("style", "width:400px");
     X.setAttribute("maxlength", 40);
+    X.setAttribute("style","resize:none");
+    X.setAttribute("class","classtest");
     mon_input_texte.appendChild(X) ;
 }
